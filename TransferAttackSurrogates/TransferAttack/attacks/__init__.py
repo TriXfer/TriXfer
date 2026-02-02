2cb82dddbcc928816b9ec639fb23aad698055e66
TriXfer <huan1932@purdue.edu>
2026-02-01 21:24:14 -0500

    Delete TransferAttackSurrogates directory

--- TransferAttackSurrogates/TransferAttack/attacks/__init__.py
+++
@@ -1,5 +0,0 @@
-from .pgd_attacker import PGDAttacker
-from .robust_attacker import RobustMinimaxAttacker
-from .transfer_attacker import TransferAttacker
-from .dct import *
-from .Normalize import Normalize
